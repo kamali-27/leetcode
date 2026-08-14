bb94228447a18f48107ed28e66e933a24beedcf3
Kamali R <23it027@drngpit.ac.in>
2026-08-14 16:03:17 +0530

    Create 319-BulbSwitcher

---
+++ 319-BulbSwitcher/319-BulbSwitcher.java
@@ -0,0 +1,12 @@
+// Last updated: 8/14/2026, 4:03:10 PM
+import java.util.*;
+class Solution {
+    public int bulbSwitch(int n) {
+        return (int)Math.sqrt(n);
+    }
+}
+// 1 1 1 1 1
+// 1 0 1 0 1
+// 1 0 0 0 1
+// 1 0 0 1 1
+// 1 0 0 1 0
