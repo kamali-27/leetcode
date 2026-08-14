1b8b72813bc95b5482ab73c6b2735384d92ac7b4
Kamali R <23it027@drngpit.ac.in>
2026-08-14 16:01:09 +0530

    Create 1086-DivisorGame

---
+++ 1086-DivisorGame/1086-DivisorGame.java
@@ -0,0 +1,9 @@
+// Last updated: 8/14/2026, 4:01:02 PM
+class Solution {
+    public boolean divisorGame(int n) {
+        if(n%2==0)
+        return true;
+        else
+        return false;
+    }
+}
