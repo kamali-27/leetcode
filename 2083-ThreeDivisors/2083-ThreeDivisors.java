ec6c4183b674deefbbd3d206b7c16bad46525c3b
Kamali R <23it027@drngpit.ac.in>
2026-08-14 15:59:23 +0530

    Create 2083-ThreeDivisors

---
+++ 2083-ThreeDivisors/2083-ThreeDivisors.java
@@ -0,0 +1,16 @@
+// Last updated: 8/14/2026, 3:59:16 PM
+class Solution {
+    public boolean isThree(int n) {
+        int count=0;
+        for(int i=1;i<=n;i++){
+            if(n%i==0){
+                count++;
+            }
+        }
+        if(count==3){
+            return true;
+        }else{
+            return false;
+        }
+    }
+}
